cbdde8c52e3040999c6e5c0da38aabbdbc908498
hosuak <tndk1966@naver.com>
2024-08-11 22:17:45 +0900

    변수 제거

--- dev/variable.tf
+++ dev/variable.tf
@@ -7,12 +7,3 @@ variable "instance_type" {
   type = string
   default = "t2.micro"
 }
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}
-
