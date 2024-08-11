10a70096a64670e33c7bef525be7bfc5445c487e
hosuak <tndk1966@naver.com>
2024-08-11 20:41:14 +0900

    init

---
+++ dev/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {    # 필요한 제공자(provider) 지정
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region                 = "ap-northeast-2"
+}
